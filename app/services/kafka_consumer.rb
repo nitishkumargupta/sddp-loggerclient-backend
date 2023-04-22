22e88f69c3e6adeb4f6a4ac08e200185b9399c3d
osamaeshmilh <osamaeshmilh@gmail.com>
2023-04-22 20:56:43 +0100

    test

--- app/services/kafka_consumer.rb
+++ app/services/kafka_consumer.rb
@@ -5,7 +5,7 @@ class KafkaConsumer
     @topic = topic
     @config = {
       'bootstrap.servers': 'localhost:9092', # Replace with your Kafka broker addresses
-      'group.id': 'kafka-app-consumer',
+      'group.id': 'http-logs-group',
       'auto.offset.reset': 'latest'
     }
   end
